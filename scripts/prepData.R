5c5f93fe647ec1e78e79f9505cb6a37fbc247ad1
Mothes <ccmothes@colostate.edu>
2022-10-19 14:09:14 -0600

    incorporate new Kampf data, add date range to table

---
+++ scripts/prepData.R
@@ -0,0 +1,65 @@
+# prep datasets for shinyapp
+
+library(dplyr)
+library(readr)
+library(stringr)
+
+# clean datasets to add availble data and date range column for table
+
+weather_data <- readRDS("app/data/weather_update.RDS") %>% 
+  rename(Site = id, Date = date, long = longitude, lat = latitude) %>% 
+  mutate(source = "NOAA") %>% 
+  mutate(precip = if_else(!(is.na(Precipitation)), "Precipitation", ""),
+         temp = if_else(if_any(c("Average_temp", "Maximum_temp", "Minimum_temp"), ~!is.na(.)), "Temperature", ""),
+         snow = if_else(if_any(c("Snowfall", "Snow_depth"), ~!is.na(.)), "Snow", ""),
+         data_available = paste(precip, temp, snow))
+
+
+qual_vars <- c("water_temp_C", "Chla", "Turbidity", "Conductivity", "DOC", "DTN", "pH",
+               "ANC", "SC", "Na", "NH4", "K", "Mg", "Ca", "F", "Cl",
+               "NO3", "PO4", "SO4")
+
+#read in updated file
+water_data <- readRDS("app/data/water_data_update.RDS") %>% arrange(Date) %>% 
+  mutate(Snow_depth = Snow_depth*10) %>% 
+  mutate(p = if_else(!(is.na(precip_mm)), "Precipitation", ""),
+         temp = if_else(if_any(c("Average_temp", "Soil_temp"), ~!is.na(.)), "Temperature", ""),
+         stream = if_else(if_any(c("stage_cm", "discharge_Ls"), ~!is.na(.)), "Streamflow", ""),
+         snow = if_else(!(is.na(Snow_depth)), "Snow", ""),
+         wq = if_else(if_any(all_of(qual_vars), ~!is.na(.)), "Water Quality", ""),
+         data_available = paste(p, temp, stream, snow, wq))
+
+
+
+# get available data for each unique site for water and weather
+data_avail_water <- water_data %>% group_by(Site) %>% 
+  summarise(across(c("p","stream", "temp", "snow", "wq"), ~paste(unique(.), collapse = ""))) %>% 
+  mutate(data_available = paste(p, stream, temp, snow, wq)) %>% 
+  dplyr::select(Site, data_available)
+
+
+data_avail_weather <- weather_data %>% group_by(Site) %>% 
+  summarise(across(c("precip","snow","temp"), ~paste(unique(.), collapse = ""))) %>% 
+  mutate(data_available = paste(precip, snow, temp)) %>% 
+  dplyr::select(Site, data_available)
+
+#combine water and weather sites for table, add date range
+
+sites_water <- water_data %>%
+  group_by(Site) %>%
+  mutate(start_date = min(Date),
+         end_date = max(Date)) %>%
+  ungroup() %>%
+  distinct(Site, .keep_all = TRUE) %>% dplyr::select(Site, source, long, lat, start_date, end_date) %>%
+  left_join(data_avail_water, by = "Site")
+
+sites_weather <- weather_data %>% distinct(Site, .keep_all = TRUE) %>% dplyr::select(Site, source, long, lat) %>% 
+  left_join(data_avail_weather, by = "Site") 
+
+sites <- bind_rows(sites_water, sites_weather)
+
+#clean up, remove extra columns from weather and water data
+weather_data <- weather_data %>% dplyr::select(-c(precip, snow, temp))
+
+water_data <- water_data %>% dplyr::select(-c(p, temp, stream, snow, wq)) %>% 
+  rename(Precipitation = precip_mm)
--- scripts/update_data.R
+++ scripts/update_data.R
@@ -15,9 +15,9 @@ dat <- readRDS("data/water_data_update.RDS")
 
 #function to convert q_mm to L/s for discharge
 
-discharge_convert <- function(q_mm, watershed){
+discharge_convert <- function(q_mm, watershed_data, watershed){
   
-  area_km <- watershed_area %>%
+  area_km <- watershed_data %>%
               filter(NAME == watershed) %>% 
               pull(area_km2)
   
@@ -43,10 +43,96 @@ stream_daily <- read_csv("data/stephanie/hydroshare_update/stream_daily.csv") %>
                names_sep = "_",
                values_drop_na = TRUE) %>% 
   rename(q_mm = Q, stage_cm = stage) %>% 
-  #Remove mtn campus for now
-  filter(site != "mtcampus")
-  
-stream_daily %>% 
+  #Remove mtn campus for now (no data on watershed area)
+  filter(site != "mtcampus") %>% 
   rowwise() %>% 
-  mutate(discharge_Ls = discharge_convert(q_mm = q_mm, watershed = site))
+  mutate(discharge_Ls = discharge_convert(q_mm = q_mm, watershed_data = watershed_area, watershed = site))
 
+
+# daily precip
+precip_daily <- read_csv("data/stephanie/hydroshare_update/P_daily.csv") %>% 
+  pivot_longer(!Date, 
+               names_to = "site",
+               values_to = "precip_mm") %>% 
+  #remove mtcampus for now
+  filter(site != "mtcampus")
+
+
+# pull daily files to get temp and snow vars
+files <- list.files("data/stephanie/hydroshare_update/met/", full.names = TRUE)
+
+site_names <- c("bighorn", "dry", "joewright_burn", "joewright", "mtcampus", "tunnel")
+
+met <- vector("list", length = length(files))
+
+for (i in 1:length(files)){
+  
+  met[[i]] <- read_csv(files[i]) %>% 
+    mutate(site = site_names[i]) %>% 
+    dplyr::select(site, contains(c("date", "Ta", "Ts_5", "snow")))
+  
+}
+
+met_daily <- bind_rows(met) %>% 
+  #remove mtcampus again for now
+  filter(site != "mtcampus") %>% 
+  dplyr::select(site, Date = date, Average_temp = Ta, Snow_depth = snow, Soil_temp = Ts_5)
+
+
+#combine all and tie to coords
+
+site_coords <- read_csv("data/stephanie/hydroshare_update/metadata.csv") %>%
+  filter(Name != "mtcampus") %>% 
+  distinct(Name, Longitude, Latitude, .keep_all = TRUE) %>% 
+  filter(Type == "stage" | Name %in% c("michigan", "joewright") & Type == "rain" | Name == "joewright_burn" & Type == "Ta") %>%  
+  dplyr::select(site = Name, long = Longitude, lat = Latitude)
+
+all_daily <- met_daily %>% 
+  full_join(precip_daily, by = c("Date", "site")) %>% 
+  full_join(stream_daily, by = c("Date", "site")) %>%
+  left_join(site_coords, by = "site") %>% 
+  mutate(Date = as.Date(as.POSIXct(Date, format = "%m/%d/%Y")),
+         lat = as.numeric(lat)) %>% 
+  mutate(Site = case_when(site == "bighorn" ~ "Bighorn Creek",
+                          site == "dry" ~ "Dry Creek",
+                         site == "joewright_burn" ~ "Joe Wright Reservoir Burned",
+                         site == "joewright" ~ "Joe WRight Reservoir Unburned",
+                         site == "tunnel" ~ "Tunnel",
+                         site == "aspen" ~ "Aspen Glenn",
+                         site == "greyrock" ~ "Grey Rock",
+                         site == "mill" ~ "Mills Creek", 
+                         site == "skin" ~ "Skin Gulch",
+                         site == "bl4" ~ "Blue Lake 4",
+                         site == "montgomery" ~ "Montgomery",
+                         site == "michigan" ~ "Michigan River",
+                         site == "dadd" ~ "Dadd Gulch",
+                         site == "hewlett" ~ "Hewlett Gulch",
+                         site == "hill" ~ "Hill Gulch",
+                         site == "michiganditch" ~ "Michigan Ditch",
+                         site == "washout" ~ "Washout"),
+         source = "CSU_Kampf") %>% 
+  dplyr::select(Site, source, Date, long, lat, Average_temp, Snow_depth, Soil_temp, precip_mm, 
+                stage_cm, discharge_Ls) %>% 
+  #filter out NA dates
+  filter(!is.na(Date))
+ 
+#rename site's to match dat
+#rename columns to match and select
+#add source column
+#NOTE that there may be two sets of coords for ~2 sites, fix in app script?
+
+
+#combine to full dataset
+water_data_update <- bind_rows(dat, all_daily) %>% 
+  #some duplicates with overlap in dates. Keep new dataset (which will be the second obs due to bind_row order)
+  group_by(Site, Date) %>% 
+  filter(row_number() == n()) %>% 
+  ungroup() %>% 
+  #still some NAs in Dates from original dataset
+  filter(!(is.na(Date)))
+
+
+#rename old dataset, save new one
+file.rename("data/water_data_update.RDS", "data/water_data_update_old10-22.RDS")
+saveRDS(water_data_update, "data/water_data_update.RDS")
+saveRDS(water_data_update, "app/data/water_data_update.RDS")
